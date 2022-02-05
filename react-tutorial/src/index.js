883206d0765aef68a3ab3ad0b28652f97d4a3f89
Viktoriia Cheriasova <darla.moonlight@gmail.com>
2022-02-05 18:59:00 +0200

    State

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Lesson from "./02_components/Lesson";
+import CounterButton from "./03_state/Lesson";
 
-ReactDOM.render(<Lesson />, document.getElementById('root'));
+ReactDOM.render(<CounterButton />, document.getElementById('root'));
 registerServiceWorker();
